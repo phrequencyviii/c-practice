88577c99b09b72278e50cd69622cae8e8890e409
Andrew Lawrence <phrequencyviii@icloud.com>
2023-07-17 21:56:16 -0400

    Initial commit
    first upload

---
+++ exercise.c
@@ -0,0 +1,24 @@
+// Comments exercise
+#include <stdio.h>
+
+int main(void)
+{
+    //this is a comment
+    /* this is a
+    multiline comment
+    I promise */
+
+// print output
+    printf("Hello Bungus\n");
+    printf("Bungus is my father...call me Bungus MAN\n");
+
+// output variables and types
+    char c = 'a';
+    int x = 123;
+    float f = 123.456f;
+    double d = 789.101112;
+    printf("%c\n", c);
+    printf("%c\n", x);
+    printf("%c\n", f);
+    printf("%c\n", d);
+}
